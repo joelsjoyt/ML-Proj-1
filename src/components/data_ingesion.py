58cc8b0317967d84878d4e18637072c1110cb9d4
Joel <joelsjoyt@gmail.com>
2024-02-04 06:27:25 +0530

    model traning

--- src/components/data_ingesion.py
+++ src/components/data_ingesion.py
@@ -7,8 +7,8 @@ from dataclasses import dataclass
 
 from src.exception import CustomException
 from src.logger import logging
-from src.components.data_transformation import DataTransformation
-from src.components.data_transformation import DataTransformationConfig
+from src.components.data_transformation import DataTransformation, DataTransformationConfig
+from src.components.model_trainer import ModelTrainer, ModelTrainerConfig
 
 @dataclass
 class DataIngestionConfig:
@@ -51,4 +51,7 @@ if __name__ == "__main__":
     train_data, test_data = obj.initiate_data_ingestion()
     
     data_transformation  = DataTransformation()
-    data_transformation.initiate_data_transformation(train_data, test_data)
+    train_array, test_array, _ = data_transformation.initiate_data_transformation(train_data, test_data)
+    
+    model_trainer = ModelTrainer()
+    print(model_trainer.initate_model_trainer(train_array, test_array))
